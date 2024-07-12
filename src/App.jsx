7ee42ce83bc4284d92383b9f0874f8c5f224432b
Andrii Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2024-07-12 12:20:19 +0300

    todolist with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+import { AddTodoFrom } from "./AddTodoForm";
+import { TodoItem } from "./TodoItem";
+
 export default function App() {
-  return <></>;
+  const [todos, setTodos] = useState([]);
+
+
+    const addTodo = (text) => {
+    const newTodo = { id: Date.now(), text };
+    setTodos([...todos, newTodo])
+  }
+
+  const deleteTodo = (id) => {
+    const newTodo = todos.filter(todo => todo.id !== id);
+    setTodos(newTodo);
+  }
+
+
+  return (
+    <>
+      <div className="App">
+        <h1>Todo List</h1>
+        <AddTodoFrom onAdd={addTodo} />
+        <ul>
+          {todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} onDelete={deleteTodo} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
---
+++ src/TodoItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const TodoItem = ({ todo, onDelete }) => {
+  return (
+    <li>
+      <p>{todo.text}</p>
+      <button onClick={() => onDelete(todo.id)}>Delete</button>
+    </li>
+  );
+};
